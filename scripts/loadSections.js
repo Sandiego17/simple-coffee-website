49187714c09c8ae9267e881b68bb01748af79b38
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2026-03-26 15:18:44 +0000

    feat: script file for loading sections

---
+++ scripts/loadSections.js
@@ -0,0 +1,18 @@
+const components = [
+  // I have done the chunk of the research for you guys
+  // This is where you add your files and their corresponding ids
+  // For example:
+  // { id: "navbar", file: "views/navbar.html" },
+
+  // If you still don't understand it or are not able to do the
+  // rest yourself, please let me know and I will be more than
+  // happy to help you out
+];
+
+components.forEach(component => {
+  fetch(component.file)
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById(component.id).innerHTML = data;
+    });
+});
